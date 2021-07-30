1f835656c869a5bd37925d603698c8ff2de0b089
Rached Masri <masri.rached@gmail.com>
2021-07-30 18:30:18 +0100

    Mobile Detection

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
   <div>
-    <div class="app flex flex-column">
+    <div v-if="!mobile" class="app flex flex-column">
       <Navigation />
       <div class="app-content flex flex-column">
-          <router-view />
+        <router-view />
       </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or Tablet</p>
     </div>
   </div>
 </template>
 
 <script>
-import Navigation from "./components/Navigation.vue"
+import Navigation from './components/Navigation.vue'
 export default {
+  data() {
+    return {
+      mobile: null,
+    }
+  },
   components: {
-    Navigation
-  }
+    Navigation,
+  },
+  created() {
+    this.checkScreen()
+    window.addEventListener('resize', this.checkScreen)
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth
+      if (windowWidth <= 750) {
+        this.mobile = true
+        return
+      }
+      this.mobile = false
+    },
+  },
 }
 </script>
 
@@ -39,6 +62,19 @@ export default {
     padding: 0 20px;
     flex: 1;
     position: relative;
+  }
+}
+
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #fff;
+
+  p {
+    margin-top: 16px;
   }
 }
 
