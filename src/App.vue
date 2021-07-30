85f2a3e4569867337386da3de921f1cbd3bef752
Rached Masri <masri.rached@gmail.com>
2021-07-30 17:24:11 +0100

    Branding Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <router-view />
+  <div>
+    <div class="app flex flex-column">
+      <Navigation />
+      <div class="app-content flex flex-column">
+          <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+import Navigation from "./components/Navigation.vue"
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
 
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
+  font-family: 'Poppins', sans-serif;
   background-color: #141625;
+}
+
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
 }
 
 button,
@@ -74,7 +103,7 @@ button,
 
 .status-button {
   &::before {
-    content: "";
+    content: '';
     width: 10px;
     height: 10px;
     border-radius: 50%;
